47f5b78d14a3ac02dad86814cd5090c8716256de
vyrak bunleang <vyrak.bunleang@gmail.com>
2015-03-02 16:36:40 -0700

    load lib files in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ Bundler.require :development
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'minitest/pride'
 require 'minitest/autorun'
+require 'rack/transaction'
 
 class Minitest::Spec
   def mock
